75b0d7d36a04546322b9fdf1aad47201f0ec3b6f
Wun Chiou <wun@utdallas.edu>
2016-03-02 11:59:51 -0600

    fix composer install

--- puphpet/puppet/modules/composer/manifests/init.pp
+++ puphpet/puppet/modules/composer/manifests/init.pp
@@ -107,7 +107,7 @@ class composer(
       $method_package = $curl_package
     }
     'wget': {
-      $download_command = 'wget https://getcomposer.org/composer.phar -O composer.phar'
+      $download_command = 'wget -qO- https://getcomposer.org/installer | ${composer::php_bin}'
       $download_require = $suhosin_enabled ? {
         false   => [ Package['wget', $php_package] ],
         default => [
@@ -143,6 +143,7 @@ class composer(
       require   => $download_require,
       creates   => "${tmp_path}/composer.phar",
       logoutput => $logoutput,
+      environment => "COMPOSER_HOME={$composer_home}",
     }
     # move file to target_dir
     file { "${target_dir}/${composer_file}":
